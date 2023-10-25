b5f5fc7f4627087e6f843199e4905b96b4136a4c
mazahir7 <91468649+mazahir7@users.noreply.github.com>
2023-10-25 20:38:24 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => nav.classList.toggle("active"));
